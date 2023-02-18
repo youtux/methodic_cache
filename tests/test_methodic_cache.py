d073bc4a3034b74e0ac39d6ba85d2df1b316e295
Alessio Bogon <778703+youtux@users.noreply.github.com>
2023-02-18 19:59:22 +0100

    Add test for no memory leaks

--- tests/test_methodic_cache.py
+++ tests/test_methodic_cache.py
@@ -1,3 +1,6 @@
+import gc
+import weakref
+
 from methodic_cache import cached_method
 
 
@@ -15,3 +18,30 @@ def test_simple():
     assert bar_cache.currsize == 1
     assert foo.bar(2) == 2
     assert bar_cache.currsize == 2
+
+
+def test_no_leaks():
+    class Foo:
+        @cached_method()
+        def bar(self, x):
+            return x | {"foo"}
+
+    foo = Foo()
+    param = frozenset()
+
+    res = foo.bar(param)
+    assert foo.bar(param) is res
+
+    foo_ref = weakref.ref(foo)
+    param_ref = weakref.ref(param)
+    res_ref = weakref.ref(res)
+
+    del foo
+    del param
+    del res
+
+    gc.collect()
+
+    assert foo_ref() is None
+    assert param_ref() is None
+    assert res_ref() is None
